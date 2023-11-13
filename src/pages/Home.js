8bb17cfb069892978388070ce7c751001d872155
Aruna Devi B <b.arunadevi30@gmail.com>
2023-11-13 16:41:03 +0530

    folder structure v1

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import App from '../components/App'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <App />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
