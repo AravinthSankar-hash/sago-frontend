ff64c726d2fe99ef97b215addec6188d3af15ef7
Aravinth Sankar <aravinthsankarhash@gmail.com>
2023-11-23 08:11:00 +0530

    filter inputs

---
+++ src/components/DateSelector.jsx
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function DateSelector({ customLabel }) {
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Container components={['DatePicker']}>
+          <DatePicker label={customLabel} />
+        </Container>
+      </LocalizationProvider>
+    </div>
+  );
+}
--- src/pages/Procurements.jsx
+++ src/pages/Procurements.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import AgGridTable from '../components/AgGridTable';
+import SearchBox from '../components/SearchBox.jsx';
+import DateSelector from '../components/DateSelector';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 function Procurements() {
   const [tableColumns, setTableColuns] = useState([]);
+  const [showNewForm, setShowNewForm] = useState(false);
   useEffect(() => {
     console.log(1);
     fetch('http://localhost:3001/columns')
@@ -14,28 +21,64 @@ function Procurements() {
         console.error('Error fetching data:', error);
       });
   }, []);
+  const showForm = (shouldShow) => {
+    setShowNewForm(shouldShow);
+  };
   return (
     <Container style={{ background: '#EBEEF0' }}>
       <Row style={{ background: 'grey', height: '56px' }}>
-        <Col>Pages Sub Header</Col>
+        <Col>
+          <ArrowBackIcon
+            style={{ cursor: 'pointer' }}
+            onClick={() => showForm(false)}
+            fontSize="medium"
+          />
+          <text>
+            {' '}
+            <span>&nbsp;&nbsp;</span>Back
+          </text>
+        </Col>
       </Row>
       <Row>
         <Col className="d-flex flex-column justify-content-center">
-          <div className="bg-info p-3 mt-2" style={{ height: '120px' }}>
-            For filters
-          </div>
-          <div>
-            <AgGridTable
-              columnDefs={[
-                { field: 'make' },
-                { field: 'model' },
-                { field: 'price' },
-                { field: 'location' },
-                { field: 'pincode' }
-              ]}
-              rowData={tableColumns}
-            />
-          </div>
+          {showNewForm ? (
+            <div>FORM COMES HERE</div>
+          ) : (
+            <div>
+              <div className="p-3 mt-2" style={{ height: '120px' }}>
+                <Row>
+                  <Col lg="4">
+                    <SearchBox></SearchBox>
+                  </Col>
+                  <Col lg="2">
+                    <DateSelector customLabel="From"></DateSelector>
+                  </Col>
+                  <Col lg="2">
+                    <DateSelector customLabel="To"></DateSelector>
+                  </Col>
+                  <Col lg="4">
+                    <Button variant="outlined" onClick={() => showForm(true)}>
+                      <AddIcon fontSize="small" />
+                      New Procurement
+                    </Button>
+                  </Col>
+                </Row>
+                <Row></Row>
+              </div>
+              <div>
+                <AgGridTable
+                  columnDefs={[
+                    { field: 'make' },
+                    { field: 'model' },
+                    { field: 'price' },
+                    { field: 'location' },
+                    { field: 'pincode' }
+                  ]}
+                  rowData={tableColumns}
+                />
+              </div>
+            </div>
+          )}
         </Col>
       </Row>
     </Container>
