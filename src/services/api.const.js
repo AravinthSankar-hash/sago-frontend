906c52edd26b50109d09447d170cd4f10f4a790e
Aravinth Sankar <aravinthsankarhash@gmail.com>
2023-12-29 07:56:50 +0530

    api wrapper

---
+++ src/services/api.const.js
@@ -0,0 +1,33 @@
+const BASE_ROUTE = '/api';
+const BASE_URL = 'http://localhost:3333';
+const SERVICES = {
+    CATALOG: {
+        ROUTES: {
+            BASE_ROUTE: '/catalogue',
+            STAFF: '/staff',
+            VEHICLE: '/vehicle',
+            PRODUCT: '/product',
+            RAWMATERIAL: '/raw-material',
+            PARTNERS: '/partner'
+        },
+        QUERY_PARAMS: {
+            CUSTOMER: 'customer',
+            BROKER: 'broker',
+            SUPPLIER: 'supplier',
+            STAFFS: 'staff',
+            PRODUCTS: 'product',
+            VEHICLES: 'vehicle',
+            RAWMATERIALS: 'raw-material',
+        }
+    }
+}
+
+const HTTP_METHODS = {
+    POST: 'POST',
+    GET: 'GET',
+    UPDATE: 'PUT',
+    DELETE: 'DELETE'
+}
+
+export { BASE_ROUTE, BASE_URL, SERVICES, HTTP_METHODS }
+
