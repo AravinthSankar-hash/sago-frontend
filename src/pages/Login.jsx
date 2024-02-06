ef599c9525273571eb72e894b1996a83b4388123
Aruna B <b.arunadevi30@gmail.com>
2024-02-06 21:57:11 +0530

    login page fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,13 +64,21 @@ export default function Login({ setToken }) {
     setShouldShowToaster(Math.random());
   };
   return (
-    <Container ref={containerRef} className="ag-theme-alpine mt-4" style={{ ...gridStyle }}>
-      <Row>
-        <Col lg={5} style={{ marginTop: '150px' }}>
+    <div className="container-fluid h-100">
+      <Row className="vh-100">
+        <Col
+          lg={4}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            gap: '40px',
+            padding: '100px'
+          }}>
           <Row>
             {' '}
             <div className="d-flex align-items-center">
-              <img src={login_logo}></img>
+              <img style={{ width: '180px' }} src={login_logo}></img>
             </div>
           </Row>
           <Row>
@@ -84,8 +92,8 @@ export default function Login({ setToken }) {
             </div>
           </Row>
           <Form onSubmit={handleSubmit(onSubmit)}>
-            <Row className="mt-4">
-              <Form.Group as={Col} lg={6}>
+            <Row className="mb-5">
+              <Form.Group as={Col} lg={12}>
                 <Form.Label>Username</Form.Label>
                 <Form.Control
                   style={inputStyle}
@@ -98,8 +106,8 @@ export default function Login({ setToken }) {
                 )}
               </Form.Group>
             </Row>
-            <Row className="mt-4">
-              <Form.Group as={Col} lg={6}>
+            <Row className="mb-5">
+              <Form.Group as={Col} lg={12}>
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                   type="password"
@@ -113,12 +121,14 @@ export default function Login({ setToken }) {
                 )}
               </Form.Group>
             </Row>
-            <Row className="mt-4">
+            <Row>
               <div>
                 <Button
+                  as={Col}
+                  lg={12}
                   type="submit"
                   style={{
-                    width: '250px',
+                    width: '100%',
                     color: '#FFFFFF',
                     border: 'none',
                     backgroundColor: '#00B7FF'
@@ -128,27 +138,43 @@ export default function Login({ setToken }) {
               </div>
             </Row>
           </Form>
-          <Row></Row>
-          <Row className="mt-5" style={{ fontSize: '12px', color: 'grey' }}>
-            {' '}
+          <Row>
+            <div>Forget Password? Contact us</div>
+          </Row>
+          <Row style={{ fontSize: '12px', color: 'grey' }}>
             <div>© 2022 ERP, All Rights Reserved</div>
           </Row>
         </Col>
-        <Col lg={7} style={{ backgroundImage: `url(${login_bg})` }} className="mt-5">
-          <Carousel>
+        <Col lg={8} style={{ backgroundImage: `url(${login_bg})` }}>
+          {/* <Carousel>
             <Carousel.Item>
               <div style={{ position: 'relative' }}>
                 <img src={slide_one} alt="First slide" style={{ width: '100%', height: '100%' }} />
-                <Carousel.Caption
-                  style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}></Carousel.Caption>
+
+                <Carousel.Caption>iuhgjkl</Carousel.Caption>
               </div>
             </Carousel.Item>
             <Carousel.Item>
               <div style={{ position: 'relative' }}>
                 <img src={slide_two} alt="First slide" style={{ width: '100%', height: '100%' }} />
-                <Carousel.Caption
-                  style={{ position: 'absolute', bottom: 0, left: 0, right: 0 }}></Carousel.Caption>
+                <Carousel.Caption>iuhgjkl dsjkhgasioj</Carousel.Caption>
               </div>
+            </Carousel.Item>
+          </Carousel> */}
+          <Carousel data-bs-theme="dark">
+            <Carousel.Item>
+              <img className="d-block w-100" src={slide_one} alt="Second slide" />
+              <Carousel.Caption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" src={slide_two} alt="Third slide" />
+              <Carousel.Caption>
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
         </Col>
@@ -157,6 +183,6 @@ export default function Login({ setToken }) {
         shouldOpen={shouldShowToaster}
         message={toasterMsg}
         backgroundColor={toasterBackground}></Toaster>
-    </Container>
+    </div>
   );
 }
